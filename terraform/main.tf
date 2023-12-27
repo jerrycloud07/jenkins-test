eab50482806059c59bc0d27a4a6fd93455ddbd7b
Ubuntu <ubuntu@ip-172-31-41-169.ap-south-1.compute.internal>
2023-12-27 01:30:18 +0000

    third commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,10 @@
 resource "aws_instance" "webhost" {
 
-ami = "ami-062df10d14676e201"
+ami = "ami-03f4878755434977f"
 instance_type = "t2.micro"
-vpc_security_group_ids = ["sg-0f73708aed121371d"]
+vpc_security_group_ids = ["sg-09e0e9057b6ba264b"]
 associate_public_ip_address = true
-key_name = "new2"
-tags ={
-  Name = "webhost"
-}
-user_data = <<EOF
-#! /bin/bash
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo systemctl start nginx 
-echo "created using terraform" | sudo tee /var/www/html/index.html
-EOF
-}  
-resource "aws_instance" "webhost" {
-
-ami = "ami-062df10d14676e201"
-instance_type = "t2.micro"
-vpc_security_group_ids = ["sg-0f73708aed121371d"]
-associate_public_ip_address = true
-key_name = "new2"
+key_name = "test"
 tags ={
   Name = "webhost"
 }
